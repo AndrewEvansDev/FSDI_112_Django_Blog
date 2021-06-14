e37d881b231f078747dfa5fe188efde9f1b8a768
Andrew Evans <78834502+AndrewEvansDev@users.noreply.github.com>
2021-06-14 03:56:09 -0700

    competency report completed

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     body = models.TextField()
+    date = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
